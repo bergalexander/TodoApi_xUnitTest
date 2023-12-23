72efbce8c02a4d406f9f383df30887eb86187934
Alexander Berg <berg.alex.h@hotmail.com>
2023-12-23 09:13:27 +0100

    Database set up, with controllers and models

---
+++ Todo_API/Data/TodoContex.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo_API.Models;
+
+namespace Todo_API.Data
+{
+    public class TodoContex : DbContext
+    {
+        public TodoContex(DbContextOptions<TodoContex> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Todo_API/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo_API.Models
+{
+    public class TodoItem
+    {
+        public int ID { get; set; }
+        public string? Text { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
