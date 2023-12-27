8951c17eee80614a3b29ebe6f72835f7f3375eac
Alexander Berg <berg.alex.h@hotmail.com>
2023-12-27 12:55:37 +0100

    Test for get notes

--- Todo_API_Test/TodoTest.cs
+++ Todo_API_Test/TodoTest.cs
@@ -43,9 +43,11 @@ namespace Todo_API_Test
         [Fact]
         public async Task Should_Return_Null()
         {
+            //Arrange
             TodoItem? note = null;
             var controller = new TodoController(_database);
-
+            
+            //Act and Assert
             await Assert.ThrowsAsync<ArgumentNullException>(() => controller.AddNote(note));
         }
 
@@ -91,7 +93,7 @@ namespace Todo_API_Test
             Assert.True(note1.IsDone);
             Assert.True(note2.IsDone);
 
-            //Test to if they are done should result in not done
+            //Test if they are done should result in not done when call again
             //Act
             await controller.ToggelAll();
 
@@ -120,8 +122,31 @@ namespace Todo_API_Test
             Assert.Null(itemInDatabaseNote2);
             var itemInDatabaseNote3 = _database.TodoItems.Find(note3.ID);
             Assert.Null(itemInDatabaseNote3);
+        }
+        [Fact]
+        public async Task Should_Return_Todo_In_Database()
+        {
+            //Arrange
+            var controller = new TodoController(_database);
+            var note1 = new TodoItem(8, "title", "description", new DateTime(2023 - 12 - 26), false);
+            var note2 = new TodoItem(9, "title2", "description2", new DateTime(2023 - 12 - 26), true);
+            var note3 = new TodoItem(10, "title3", "description3", new DateTime(2023 - 12 - 26), true);
+            await controller.AddNote(note1);
+            await controller.AddNote(note2);
+            await controller.AddNote(note3);
 
+            //Act
+           var result = await controller.GetNotes(true);
 
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+
+            var notes = okResult.Value as List<TodoItem>; 
+            Assert.NotNull(notes);
+            Assert.Equal(2, notes.Count); // Check so the list is actual 2
+            Assert.Equal(notes[0].Title, note2.Title); //Check so the note on index 1 is the same as 2. 
         }
     }
 }
